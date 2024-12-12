7fd26a31cbc001ceaa575419c618b9ac9c946367
Akshat Dubey <akshatdubey0808@gmail.com>
2024-12-12 16:12:59 +0530

    fix: removed unused import

--- app/api/reminder/route.ts
+++ app/api/reminder/route.ts
@@ -4,32 +4,32 @@ import prisma from "@/db";
 import client from "@/mail";
 import { NextResponse } from "next/server";
 
-interface EmailRecipient {
-  email: string;
-  emailPreferences: boolean;
-}
+// interface EmailRecipient {
+//   email: string;
+//   emailPreferences: boolean;
+// }
 
-interface BondMember {
-  user: EmailRecipient;
-}
+// interface BondMember {
+//   user: EmailRecipient;
+// }
 
-interface Bond {
-  name: string;
-  members: BondMember[];
-}
+// interface Bond {
+//   name: string;
+//   members: BondMember[];
+// }
 
-interface BingoCard {
-  bond: Bond;
-}
+// interface BingoCard {
+//   bond: Bond;
+// }
 
-interface Plan {
-  id: string;
-  planDate: Date;
-  card: BingoCard;
-  cell: {
-    activity: string;
-  };
-}
+// interface Plan {
+//   id: string;
+//   planDate: Date;
+//   card: BingoCard;
+//   cell: {
+//     activity: string;
+//   };
+// }
 
 interface SendEmailProps {
   planName: string;
